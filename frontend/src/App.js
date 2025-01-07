afed0208ddea48bd228b8a548d47ab029f24272d
Sky Yin <sky.yin@gmail.com>
2025-01-07 00:27:33 -0800

    Form created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -70,6 +70,63 @@ const App = () => {
               value={formData.amount}
             />
           </div>
+
+          <div className="mb-3">
+            <label htmlFor="category" className="form-label">
+              Category
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="category"
+              name="category"
+              onChange={handleInputChange}
+              value={formData.category}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="description" className="form-label">
+              Description
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="description"
+              name="description"
+              onChange={handleInputChange}
+              value={formData.description}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="is_income" className="form-label">
+              Income? 
+            </label>
+            <input
+              type="checkbox"
+              id="is_income"
+              name="is_income"
+              onChange={handleInputChange}
+              value={formData.is_income}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="date" className="form-label">
+              Date
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="date"
+              name="date"
+              onChange={handleInputChange}
+              value={formData.date}
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">Submit</button>
         </form>
       </div>
     </div>
